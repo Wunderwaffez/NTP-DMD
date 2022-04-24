e5617dbc3cae044d422aab6ed1e7bafa0ced06e6
Wunderwaffez <40244340+Wunderwaffez@users.noreply.github.com>
2022-04-24 21:17:45 +0300

    Add files via upload

--- NTP-DMD.ino
+++ NTP-DMD.ino
@@ -4,33 +4,82 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-#define SOFTAP_SSID "NTP"
-#define SOFTAP_PASSWORD "87654321"
-#define SERVER_PORT 2000
+#include <SPI.h>
+#include <DMDESP.h>
+#include <fonts/angka6x13.h>
+#include <fonts/SystemFont5x7.h>
+
+DMDESP dmd(1,1);
 
 const char *ssid     = "bugs";
 const char *password = "04546412889";
 
+int last_second = 0;
+
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 10800, 60000);
+NTPClient ntp(ntpUDP, "europe.pool.ntp.org", 10800, 300000);
 
 void setup() {
-  Serial.begin(115200);
-  
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAPConfig(IPAddress(192, 168, 35, 35), IPAddress(192, 168, 35, 35), IPAddress(255, 255, 255, 0));
-  WiFi.softAP(SOFTAP_SSID, SOFTAP_PASSWORD, 6, 0);
-  WiFi.setAutoReconnect(true);
-  WiFi.begin(ssid, password);
 
-  ArduinoOTA.begin();
-  server.begin();
-  timeClient.begin();
+  last_second = millis();
   
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
+  ArduinoOTA.setHostname("NTP");
+
+  dmd.start();
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  ArduinoOTA.begin();
+  ntp.begin();
+  
+  dmd.setBrightness(1);
+  dmd.setFont(SystemFont5x7);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    ESP.restart();
+  }
+    Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
-  timeClient.update();
-
+  dmd.loop();
   
+  ArduinoOTA.handle();
+
+  dmd.setFont(SystemFont5x7);
+  
+  if (millis() / 1000 % 2 == 0)
+  {
+    dmd.drawChar(14, 5, ':');
+  }
+  else
+  {
+    dmd.drawChar(14, 5, ' ');
+  }
+
+  if (millis() - last_second > 1000){
+  last_second = millis();   
+  ntp.update();
+  
+  int saat = ntp.getHours();
+  int dakika = ntp.getMinutes();
+  String saat0;
+  String dakika0;
+  
+  dakika0 = String(dakika);
+    if (dakika < 10) {dakika0 = '0' + dakika0;}
+    else dakika0 = dakika0;
+
+    saat0 = String (saat);
+    if (saat < 10) {saat0 = ("0") + (saat0);}
+    else {saat0 = saat0;}
+
+  dmd.setFont(angka6x13);
+
+  dmd.drawText(0, 1, String(saat0));
+  dmd.drawText(19, 1, String(dakika0));
+  
+  }
 }
